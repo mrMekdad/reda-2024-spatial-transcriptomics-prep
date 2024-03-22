684bc9700ecbe91dae426563e16771a95c57f116
mrMekdad <mekdadr6@gmail.com>
2024-03-22 13:13:00 +0100

    spatial-transcriptomics-prep: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2024-spatial-transcriptomics-prep",
+    version="0.1.0",
+    description="Preparation utilities for spatial transcriptomics sample metadata and region grids.",
+    author="Red@",
+    packages=["spatial_transcriptomics_prep"],
+    package_dir={"": "src"},
+)
